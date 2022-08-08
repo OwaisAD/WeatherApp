f7d49db605cb5c7ccf4ba38c04db1e81a4f462eb
Owais <33609590+OwaisAD@users.noreply.github.com>
2022-08-08 16:22:43 +0200

    Add files via upload

---
+++ WeatherApp/script.js
@@ -0,0 +1,65 @@
+// Accessing the different divs I want to manipulate in the DOM
+const cityNameDiv = document.getElementById("city-name")
+const weatherTypeDiv = document.getElementById("weather-type")
+const weatherDescriptionDiv = document.getElementById("weather-description")
+const tempDiv = document.getElementById("temp")
+const feelsLikeDiv = document.getElementById("feels-like")
+const humidityDiv = document.getElementById("humidity")
+const minTempDiv = document.getElementById("min-temp")
+const maxTempDiv = document.getElementById("max-temp")
+const lastUpdatedDiv = document.getElementById("last-updated")
+
+// other variables: API_KEY and default city that I want to display on website load
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+let cityDefault = "Copenhagen"
+
+/**
+ * API url
+ * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
+ */
+// Method to acces 
+const getWeatherData = async (city) => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    const response = await fetch(URL)
+    const data = await response.json()
+    return data
+}
+
+// run first time loading website
+
+/**
+ * Retrieve city input and get the weather data
+ * HINT: Use the promise returned from getWeatherData()
+ */
+const searchCity = async () => {
+    const city = document.getElementById('city-input').value;
+    const data = await getWeatherData(city) //.then(value => console.log(value))
+    showWeatherData(data)
+}
+
+/**
+ * Show the weather data in HTML
+ * HINT: make sure to console log the weatherData to see how the data looks like
+ */
+const showWeatherData = (weatherData) => {
+    console.log(weatherData, "✅")
+    cityNameDiv.innerText = document.getElementById("city-input").value
+    weatherTypeDiv.innerText = weatherData.weather[0].main
+    weatherDescriptionDiv.innerText = weatherData.weather[0].description
+    tempDiv.innerText = weatherData.main.temp
+    feelsLikeDiv.innerText = weatherData.main.feels_like
+    humidityDiv.innerText = weatherData.main.humidity
+    minTempDiv.innerText = weatherData.main.temp_min
+    maxTempDiv.innerText = weatherData.main.temp_max
+    //The API does not give any data in regards to when the forecast last updated, so I simply display the current time
+    const currTime = new Date()
+    lastUpdatedDiv.innerText = currTime.toLocaleTimeString()
+}
+
+// a little trick that waits for the website to be fully loaded
+// then it fetches data for Copenhagen, which I've set to be the default city
+document.addEventListener('DOMContentLoaded', async function () {
+    const data = await getWeatherData(cityDefault)
+    showWeatherData(data)
+    cityNameDiv.innerText = cityDefault
+}, false);
